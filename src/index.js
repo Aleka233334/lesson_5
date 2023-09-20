1506c3135d362bdc3281c9f6b014feb30eee47e2
oleg233334 <142994486+oleg233334@users.noreply.github.com>
2023-09-20 22:36:27 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+const inputString = "Hello, JS!";
+const reversed = reverseString(inputString);
+console.log(reversed);
